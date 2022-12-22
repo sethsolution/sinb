69dca586fe275b93370b7d42eaaa4c031883e327
Henry Taby <henry.taby@gmail.com>
2022-12-22 19:49:41 -0400

    apidoc en sinbio api configuración de swagger y otros

--- html/sinbioapi/app/Http/Controllers/Api/V1/CiteController.php
+++ html/sinbioapi/app/Http/Controllers/Api/V1/CiteController.php
@@ -15,6 +15,32 @@ class CiteController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    /**
+     * @OA\Get(
+     *     path="/api/v1/cites/",
+     *     summary="Lista de solicitudes CITES generadas",
+     *     description="Devuelve la lista de los datos de todas las solicitudes cites generadas en el sistema",
+     *     operationId="v1getCitesList",
+     *     tags={"V1"},
+     *     security={{"bearerAuth":{}}},
+
+     *     @OA\Response(
+     *         response=200,
+     *         description="Json con datos de la lista de solicitudes CITES",
+     *         @OA\JsonContent()
+     *
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="No autentificado",
+     *         @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *         )
+     *     )
+     *
+     * )
+     *
+     */
     public function index()
     {
         return CiteResource::collection(Cite::all());
@@ -25,6 +51,41 @@ class CiteController extends Controller
      *
      * @param  \App\Models\Cite  $cite
      * @return \Illuminate\Http\Response
+     */
+    /**
+     * @OA\Get(
+     *     path="/api/v1/cites/{citesId}",
+     *     summary="Buscar una solicitud CITES por ID",
+     *     description="Devuelve la información de una ficha de una solicitud CITES específico",
+     *     operationId="v1getCites",
+     *     tags={"V1"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *      name="citesId",in="path",required=true,
+     *      description="ID de la solicitud CITES",
+     *      @OA\Schema(type="integer",default="1",format="int64")
+     *     ),
+     *
+     *     @OA\Response(
+     *         response=200,
+     *         description="Se optiene los datos con éxito",
+     *          @OA\JsonContent()
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="No se encontraron datos",
+     *         @OA\JsonContent()
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="No autentificado",
+     *         @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *         )
+     *     )
+     *
+     * )
+     *
      */
     public function show(Cite $cite)
     {
